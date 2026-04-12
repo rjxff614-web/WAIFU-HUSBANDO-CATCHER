e42a164fe35b44b554f3d4dcb25bcd1af09ab3b5
rjxff614-web <rjxff614@gmail.com>
2026-04-12 21:39:49 +0530

    Update __init__.py

--- shivu/__init__.py
+++ shivu/__init__.py
@@ -1,6 +1,6 @@
-import logging  
+import logging
 import os
-from pyrogram import Client 
+from pyrogram import Client
 from telegram.ext import Application
 from motor.motor_asyncio import AsyncIOMotorClient
 
@@ -11,33 +11,34 @@ logging.basicConfig(
 )
 
 logging.getLogger("apscheduler").setLevel(logging.ERROR)
-logging.getLogger('httpx').setLevel(logging.WARNING)
+logging.getLogger("httpx").setLevel(logging.WARNING)
 logging.getLogger("pyrate_limiter").setLevel(logging.ERROR)
+
 LOGGER = logging.getLogger(__name__)
 
-from shivu.config import Development as Config
-
+from shivu.config import Config
 
 api_id = Config.api_id
 api_hash = Config.api_hash
 TOKEN = Config.TOKEN
 GROUP_ID = Config.GROUP_ID
-CHARA_CHANNEL_ID = Config.CHARA_CHANNEL_ID 
-mongo_url = Config.mongo_url 
-PHOTO_URL = Config.PHOTO_URL 
-SUPPORT_CHAT = Config.SUPPORT_CHAT 
+CHARA_CHANNEL_ID = Config.CHARA_CHANNEL_ID
+mongo_url = Config.mongo_url
+PHOTO_URL = Config.PHOTO_URL
+SUPPORT_CHAT = Config.SUPPORT_CHAT
 UPDATE_CHAT = Config.UPDATE_CHAT
-BOT_USERNAME = Config.BOT_USERNAME 
+BOT_USERNAME = Config.BOT_USERNAME
 sudo_users = Config.sudo_users
-OWNER_ID = Config.OWNER_ID 
+OWNER_ID = Config.OWNER_ID
 
 application = Application.builder().token(TOKEN).build()
 shivuu = Client("Shivu", api_id, api_hash, bot_token=TOKEN)
+
 lol = AsyncIOMotorClient(mongo_url)
-db = lol['Character_catcher']
-collection = db['anime_characters_lol']
-user_totals_collection = db['user_totals_lmaoooo']
+db = lol["Character_catcher"]
+collection = db["anime_characters_lol"]
+user_totals_collection = db["user_totals_lmaoooo"]
 user_collection = db["user_collection_lmaoooo"]
-group_user_totals_collection = db['group_user_totalsssssss']
-top_global_groups_collection = db['top_global_groups']
-pm_users = db['total_pm_users']
+group_user_totals_collection = db["group_user_totalsssssss"]
+top_global_groups_collection = db["top_global_groups"]
+pm_users = db["total_pm_users"]
